9e4bf357e1f5c864aaa6eed377af009cb40d46dc
karenhan2022 <100669296+karenhan2022@users.noreply.github.com>
2023-03-08 05:22:41 -0300

    Agregada función para validar datos del formulario

--- codigojs.js
+++ codigojs.js
@@ -1,13 +1,13 @@
 
-// Función para cambiar sección formulario por sección mensaje 
-document.getElementById("form-button").onclick = function () {
-    document.getElementById("form-full").style.display = "none";
-    document.getElementById("form-message").style.display = "block";
-}
+// Función para cambiar sección Contacto por formulario de contacto
 
-// Función para cambiar sección mensaje por sección formulario 
-document.getElementById("form-back").onclick = function () {
+document.getElementById("form-show").onclick = function () {
     document.getElementById("form-message").style.display = "none";
     document.getElementById("form-full").style.display = "block";
 }
 
+// Función para cambiar sección mensaje por sección formulario 
+    document.getElementById("form-back").onclick = function () {
+    document.getElementById("form-full").style.display = "none";
+    document.getElementById("form-message").style.display = "block";
+}
